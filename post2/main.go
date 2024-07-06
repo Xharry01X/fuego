e7bc89113d48f1f2872fe7287711b0902984ecb8
harshit-506 <supriyamohapatra658@gmail.com>
2024-07-06 13:36:20 +0530

    Done with connecting mongoDB

---
+++ post2/main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"context"
+	"time"
+
+	"github.com/go-fuego/fuego"
+	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/bson"
+	"golang.org/x/crypto/bcrypt"
+	"github.com/dgrijalva/jwt-go"
+)
+
+type UserInput struct {
+	Username string `json:"username" validate:"required"`
+	Password string `json:"password" validate:"required"`
+}
+
+type UserOutput struct {
+	Username string `json:"username"`
+	Token    string `json:"token"`
+}
+
+var validate *validator.Validate
+var userCollection *mongo.Collection
+var jwtKey = []byte("your_secret_key")
+
+func main() {
+	validate = validator.New()
+
+	// Connect to MongoDB
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		panic(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		panic(err)
+	}
+
+	userCollection = client.Database("Fuego").Collection("users")
+
+	s := fuego.NewServer(
+		fuego.WithAddr(":4000"),
+	)
+
+	fuego.Post(s, "/user", func(c *fuego.ContextWithBody[UserInput]) (UserOutput, error) {
+		body, err := c.Body()
+		if err != nil {
+			return UserOutput{}, err
+		}
+
+		// Validate the body
+		err = validate.Struct(body)
+		if err != nil {
+			return UserOutput{}, err
+		}
+
+		// Hash the password
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(body.Password), bcrypt.DefaultCost)
+		if err != nil {
+			return UserOutput{}, err
+		}
+
+		// Save user to MongoDB
+		user := bson.D{
+			{Key: "username", Value: body.Username},
+			{Key: "password", Value: string(hashedPassword)},
+		}
+
+		_, err = userCollection.InsertOne(context.TODO(), user)
+		if err != nil {
+			return UserOutput{}, err
+		}
+
+		// Create JWT token
+		token, err := generateJWT(body.Username)
+		if err != nil {
+			return UserOutput{}, err
+		}
+
+		return UserOutput{
+			Username: body.Username,
+			Token:    token,
+		}, nil
+	})
+
+	s.Run()
+}
+
+func generateJWT(username string) (string, error) {
+	claims := &jwt.StandardClaims{
+		ExpiresAt: time.Now().Add(time.Hour * 72).Unix(),
+		Issuer:    username,
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(jwtKey)
+}
